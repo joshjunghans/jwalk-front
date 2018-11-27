e63c94afd609c20e482dc73a7eec9c159e5f2275
joshjunghans <josh.junghans@gmail.com>
2018-11-27 09:38:11 -0600

    testing to docs

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ sass.compiler = require('node-sass');
 gulp.task('sass', function () {
   return gulp.src('./lib/css/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(gulp.dest('./docs/dist/css'));
 });
 
 gulp.task('sass:watch', function () {
