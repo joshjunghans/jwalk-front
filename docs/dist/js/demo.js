bc9b03e2f5399e8a02d749a9ab11aa8a6cd141e2
joshjunghans <josh.junghans@gmail.com>
2018-11-28 08:32:35 -0600

    demo build

--- docs/dist/js/demo.js
+++ docs/dist/js/demo.js
@@ -1,5 +1,26 @@
 window.onload = (function() {
 
+    var Home = Vue.component('home', {
+        template : '#template-home'
+    });
+
+    var Typography = Vue.component('typography', {
+        template: '#template-typography'
+    });
+
+    var routes = [
+        { path: '/', component : Home },
+        { path: '/typography', component : Typography },
+    ];
+
     console.log('hello');
+
+    var router = new VueRouter({
+        routes: routes
+    });
+
+    var app = new Vue({
+        router
+    }).$mount('#jwalk-demo')
 
 })();
